de2737f3794efcee2b699418cf7c014371e038f3
Jeff Clough <jeff@cloughcottage.com>
2021-07-17 19:59:11 -0400

    End of chapter 2
    * Templates
    * Using "if" and "for" statements in templates
    * Template inheritance
    * Block definitions and references

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 
 import os
+from flask import render_template
 from flask import send_from_directory
 
 @app.route('/favicon.ico')
@@ -14,6 +15,15 @@ def favicon():
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Hello, World!"
-
-
+  user=dict(username='Jeff')
+  posts=[
+    dict(
+      author=dict(username='John'),
+      body="Beautiful Day in Portland!",
+    ),
+    dict(
+      author=dict(username='Susan'),
+      body="The Avergers Movie Was So Cool!"
+    ),
+  ]
+  return render_template('index.html',title='Home',user=user,posts=posts)
