62dc0104828cddf379e704b19e015b010e6c86ed
Jeff Clough <jeff@cloughcottage.com>
2021-08-06 23:07:37 -0400

    Add database models for data persistence
    Resume in the middle of chapter 4
    https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database
    
    Start at "Creating The Migration Repository."

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import os
 
+basedir=os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
   SECRET_KEY=os.environ.get('SECRET_KEY') or "okay for dev, but set real key for prod"
+  SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or 'sqlite:///'+os.path.join(basedir,'app.db')
+  SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+
